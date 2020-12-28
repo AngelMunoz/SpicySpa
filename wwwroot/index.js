2c89a77be6d1032da906f3cf3e4aa154fa8dd15d
Angel D. Munoz <scythes.lol@gmail.com>
2020-12-28 01:53:40 -0700

    update click handlers

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -1,12 +1,3 @@
-document.body.addEventListener("htmx:configRequest", function (evt) {
-  var form = document.querySelector('input[name="__RequestVerificationToken"]');
-  if (
-    form &&
-    ["post", "patch", "put", "delete"].includes(evt.detail.verb.toLowerCase())
-  ) {
-    evt.detail.headers["XSRF-TOKEN"] = form.value;
-  }
-});
 
 /**
  * @this {HTMLAnchorElement}
@@ -21,11 +12,65 @@ function navbarClick(event) {
     menu.classList.toggle("is-active");
   }
 }
+/**
+ * @this {HTMLDivElement}
+ * @param {MouseEvent} event 
+ */
+function removeElement(event) {
+  const target = event.currentTarget;
+  /**
+   * @type {HTMLElement}
+   */
+  const parent = target.parentElement;
+  target.removeEventListener("click", removeElement);
+  parent.remove();
+}
+
 
 document.addEventListener("DOMContentLoaded", function (event) {
   var items = Array.from(document.querySelectorAll(".navbar-burger"));
   items.forEach(function (navbar) {
     navbar.addEventListener("click", navbarClick);
+  });
+
+  var deletes = Array.from(document.querySelectorAll(".delete"))
+  deletes.forEach(function(del) {
+    del.addEventListener("click", removeElement);
+  });
+});
+
+document.body.addEventListener("htmx:configRequest", function (evt) {
+  var form = document.querySelector('input[name="__RequestVerificationToken"]');
+  if (
+    form &&
+    ["post", "patch", "put", "delete"].includes(evt.detail.verb.toLowerCase())
+  ) {
+    evt.detail.headers["XSRF-TOKEN"] = form.value;
+  }
+});
+
+document.body.addEventListener("htmx:responseError", function (evt) {
+  /**
+   * @type {HTMLElement}
+   */
+  const target = evt.detail.target;
+  if(target === document.body) {
+    const main = target.querySelector('.app-main');
+    main.innerHTML = evt.detail.xhr.response;
+  } else {
+    target.innerHTML = evt.detail.xhr.response;
+  }
+
+  if (evt.detail.elt === document.body) {
+    var items = Array.from(document.querySelectorAll(".navbar-burger"));
+    items.forEach(function (navbar) {
+      navbar.addEventListener("click", navbarClick);
+    });
+  }
+
+  var deletes = Array.from(document.querySelectorAll("button.delete"))
+  deletes.forEach(function(del) {
+    del.addEventListener("click", removeElement);
   });
 });
 
@@ -36,4 +81,9 @@ document.body.addEventListener("htmx:load", function (evt) {
       navbar.addEventListener("click", navbarClick);
     });
   }
+
+  var deletes = Array.from(document.querySelectorAll("button.delete"))
+  deletes.forEach(function(del) {
+    del.addEventListener("click", removeElement);
+  });
 });
